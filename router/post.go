580709cbaab26ca44bc2b90c9ca8e512dfba9c2d
koldin108902 <alex108902@naver.com>
2022-06-02 12:58:57 +0900

    change: db code

--- router/post.go
+++ router/post.go
@@ -1,7 +1,13 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	_ "github.com/lib/pg"
+)
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
-
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
 }
