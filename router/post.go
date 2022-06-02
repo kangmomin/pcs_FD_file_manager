cfa67d79e0262b09360d19bf0a24b57d02a1ab16
koldin108902 <alex108902@naver.com>
2022-06-03 06:31:43 +0900

    add: database connection

--- router/post.go
+++ router/post.go
@@ -1,13 +1,25 @@
 package router
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	_ "github.com/lib/pg"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+	godotenv.Load("./.env")
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := sql.Open("postgres", dbinfo)
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+
 }
