5dbcaba14f417b54a7fcade74b49c4f97188093e
koldin108902 <alex108902@naver.com>
2022-06-08 04:55:00 +0900

    fixed: early db close

--- util/dbConn.go
+++ util/dbConn.go
@@ -14,14 +14,13 @@ var DB *sql.DB = connDB()
 
 func connDB() *sql.DB {
 	godotenv.Load("./.env")
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	dbinfo := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_NAME"))
 
 	db, err := sql.Open("postgres", dbinfo)
 
 	if err != nil {
 		log.Println(err)
 	}
-	defer db.Close()
 
 	return db
 }
